503052f294caded9908810b2ca0ab46a2c5d0495
Luis Angel <angelfgdeveloper@gmail.com>
2022-09-10 15:39:13 -0500

    Optimizaciones y lazy load

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router } from '@angular/router';
-import { tap } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router, CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
+import { Observable, tap } from 'rxjs';
 
 import { UsuarioService } from '../services/usuario.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(
     private usuarioService: UsuarioService,
@@ -22,6 +22,17 @@ export class AuthGuard implements CanActivate {
     // this.usuarioService.validarToken().subscribe(resp => {
     //   console.log(resp);
     // });
+    return this.usuarioService.validarToken().pipe(
+      tap(isAuth => {
+        if (!isAuth) {
+          this.router.navigateByUrl('/login');
+        }
+      })
+    ); // Retorna el valor
+  }
+
+  // Solo carga si el usuario tiene acceso a esa ruta
+  canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
     return this.usuarioService.validarToken().pipe(
       tap(isAuth => {
         if (!isAuth) {
